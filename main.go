215203b2355d9e384f99b0bde824bcaf4b6816bc
JayRGarg <jayrgarg@gmail.com>
2025-05-13 21:50:13 -0700

    add cli command map, add exit and help

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-    "fmt"
-    "strings"
+	"bufio"
+    "os"
+	"fmt"
+	"strings"
 )
 
 func cleanInput(text string) []string {
@@ -13,6 +15,63 @@ func cleanInput(text string) []string {
     return clean
 }
 
+func commandExit() error {
+    fmt.Println("Closing the Pokedex... Goodbye!")
+    os.Exit(0)
+    return nil
+}
+
+func commandHelp() error {
+    fmt.Println("Welcome to the Pokedex!")
+    fmt.Println("Usage:")
+    for _, v := range cmdMap {
+        fmt.Println(v.name+":", v.description)
+    }
+    return nil
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var cmdMap map[string]cliCommand
+
 func main() {
-    fmt.Println("Hello, World!")
+
+    cmdMap = map[string]cliCommand {
+        "help": {
+            name:        "help",
+            description: "Displays a help message",
+            callback:    commandHelp,
+        },
+        "exit": {
+            name:        "exit",
+            description: "Exit the Pokedex",
+            callback:    commandExit,
+        },
+    }
+
+
+    scanner := bufio.NewScanner(os.Stdin)
+
+    for {
+        fmt.Print("Pokedex > ")
+        scanner.Scan()
+        inputTxt := scanner.Text()
+        tokens := cleanInput(inputTxt)
+        if len(tokens) == 0 {
+            continue
+        }
+        cmd, exists := cmdMap[tokens[0]]
+        if !exists {
+            fmt.Println("Unknown command")
+        } else {
+            err := cmd.callback()
+            if err != nil {
+                fmt.Println("Error:", err)
+            }
+        }
+    }
 }
