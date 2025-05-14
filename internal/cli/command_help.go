02dad8d13fca0ebb88c40b7ddd179f9b93d094f6
JayRGarg <jayrgarg@gmail.com>
2025-05-14 16:52:51 -0700

    add sorting of help command output

--- internal/cli/command_help.go
+++ internal/cli/command_help.go
@@ -2,14 +2,27 @@ package cli
 
 import (
     "fmt"
+    "sort"
     "github.com/jayrgarg/pokedexcli/internal/config"
 )
 
 func commandHelp(cfg *config.Config) error {
     fmt.Println("Welcome to the Pokedex!")
     fmt.Println("Usage:")
-    for _, v := range GetCmdMap() {
-        fmt.Println(v.Name+":", v.Description)
-    }
+
+    commands := GetCmdMap()
+    keys := make([]string, 0, len(commands))
+	for name := range commands {
+		keys = append(keys, name)
+	}
+
+	sort.Strings(keys)
+
+	for _, name := range keys {
+        fmt.Println(commands[name].Name+":", commands[name].Description)
+	}
+    // for _, v := range GetCmdMap() {
+    //     fmt.Println(v.Name+":", v.Description)
+    // }
     return nil
 }
