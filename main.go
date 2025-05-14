a4a6925cd9f71158cee1b96ce3061e078a0fb627
JayRGarg <jayrgarg@gmail.com>
2025-05-14 11:09:43 -0700

    add map, mapb functionality

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"bufio"
-    "os"
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
+	"os"
 	"strings"
 )
 
@@ -15,13 +18,13 @@ func cleanInput(text string) []string {
     return clean
 }
 
-func commandExit() error {
+func commandExit(cfg *config) error {
     fmt.Println("Closing the Pokedex... Goodbye!")
     os.Exit(0)
     return nil
 }
 
-func commandHelp() error {
+func commandHelp(cfg *config) error {
     fmt.Println("Welcome to the Pokedex!")
     fmt.Println("Usage:")
     for _, v := range cmdMap {
@@ -30,15 +33,119 @@ func commandHelp() error {
     return nil
 }
 
+func commandMap(cfg *config) error {
+    url := cfg.Next
+    req, err := http.NewRequest("GET", url, nil)
+    if err != nil {
+        return fmt.Errorf("Error formatting request: %w", err)
+    }
+
+    client := &http.Client{}
+    res, err := client.Do(req)
+    if err != nil {
+        return fmt.Errorf("Error performing request from Pokedox API: %w", err)
+    }
+    defer res.Body.Close()
+
+    resourcesBytes, err := io.ReadAll(res.Body)
+    if err != nil {
+        return fmt.Errorf("Error reading response to bytes: %w", err)
+    }
+
+    var resourcesParsed Resources
+    err = json.Unmarshal(resourcesBytes, &resourcesParsed)
+    if err != nil {
+        return fmt.Errorf("Error Unmarshalling resources to struct: %w", err)
+    }
+
+    for _, v := range resourcesParsed.Results {
+        fmt.Println(v.Name)
+    }
+
+    cfg.Next = resourcesParsed.Next
+    cfg.Previous = resourcesParsed.Previous
+
+    return nil
+}
+
+func commandMapB(cfg *config) error {
+    url := cfg.Previous
+    if url == "" {
+        if strings.Contains(cfg.Next, "offset=0") {
+            fmt.Println("Currently haven't gone to First Page")
+            return nil
+        } else if strings.Contains(cfg.Next, "offset=20") {
+            fmt.Println("Currently on First Page, go to any of the next pages with the 'map' command")
+            return nil
+        } else {
+            return fmt.Errorf("Error: cfg.Previous empty with unexpected cfg.Next")
+        }
+    }
+
+    req, err := http.NewRequest("GET", url, nil)
+    if err != nil {
+        return fmt.Errorf("Error formatting request: %w", err)
+    }
+
+    client := &http.Client{}
+    res, err := client.Do(req)
+    if err != nil {
+        return fmt.Errorf("Error performing request from Pokedox API: %w", err)
+    }
+    defer res.Body.Close()
+
+    resourcesBytes, err := io.ReadAll(res.Body)
+    if err != nil {
+        return fmt.Errorf("Error reading response to bytes: %w", err)
+    }
+
+    var resourcesParsed Resources
+    err = json.Unmarshal(resourcesBytes, &resourcesParsed)
+    if err != nil {
+        return fmt.Errorf("Error Unmarshalling resources to struct: %w", err)
+    }
+
+    for _, v := range resourcesParsed.Results {
+        fmt.Println(v.Name)
+    }
+
+    cfg.Next = resourcesParsed.Next
+    cfg.Previous = resourcesParsed.Previous
+
+    return nil
+}
+
 type cliCommand struct {
 	name        string
 	description string
-	callback    func() error
+	callback    func(*config) error
+}
+
+type config struct {
+    Next        string
+    Previous    string
+}
+
+type Resource struct {
+    Name        string
+    Url         string
+}
+
+type Resources struct {
+    Count       int
+    Next	    string
+    Previous	string
+    Results     []Resource
 }
 
 var cmdMap map[string]cliCommand
 
 func main() {
+
+    cfg := &config{
+        Next: "https://pokeapi.co/api/v2/location-area/?offset=0&limit=20",
+        Previous: "",
+    }
 
     cmdMap = map[string]cliCommand {
         "help": {
@@ -50,6 +157,16 @@ func main() {
             name:        "exit",
             description: "Exit the Pokedex",
             callback:    commandExit,
+        },
+        "map": {
+            name:        "map",
+            description: "Displays the next 20 locations",
+            callback:    commandMap,
+        },
+        "mapb": {
+            name:        "mapb",
+            description: "Displays the previous 20 locations",
+            callback:    commandMapB,
         },
     }
 
@@ -68,9 +185,9 @@ func main() {
         if !exists {
             fmt.Println("Unknown command")
         } else {
-            err := cmd.callback()
+            err := cmd.callback(cfg)
             if err != nil {
-                fmt.Println("Error:", err)
+                fmt.Println(err)
             }
         }
     }
